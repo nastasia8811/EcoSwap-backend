1213c808bc39ea4e35a5276169598de05c9969f3
Anastasiia Melnyk <nastasia8811@gmail.com>
2024-04-19 17:30:27 +0200

    back

---
+++ config/keys_dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:
+      "mongodb+srv://redi:111@atlascluster.flcy3uy.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: '6611b081d2c5b229ac914c34',
+  nodemailerUser: 'myprojectslifestyle@gmail.com',
+  nodemailerPassword: 'myproject2023!',
+  nodemailerService: 'gmail'
+};
