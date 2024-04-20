3dc275f2b41b4b34387b7e83a140c0776e6e22ad
Melnyk Anastasiia <nastasia8811@gmail.com>
2024-04-20 15:38:16 +0200

    styles Footer

--- my-app/src/components/footer/Footer.tsx
+++ my-app/src/components/footer/Footer.tsx
@@ -1,15 +1,26 @@
 import { Box, Container } from '@mui/material';
+import {Link, NavLink} from "react-router-dom";
+import {logo} from "../Header/icons";
+import "./Footer.scss";
 const Footer = () => {
 
     return (
-        <>
             <footer className="footer">
-                <Box className="footer__subscribe-container">
                     <Container maxWidth="xl">
+                        <Box className="footer__container">
+                            <Box className="footer__container-logo">
+                                <Link to="/" className="logo">
+                                    {logo}
+                                </Link>
+                            </Box>
+                            <Box className="footer__container-menu">
+                                <p className="footer__container-menu-item">tel. +49 1517 090 81 75</p>
+                                <p className="footer__container-menu-item">email: ecoswap@gmail.com</p>
+                                <p className="footer__container-menu-item">Hamburg, Germany</p>
+                            </Box>
+                        </Box>
                     </Container>
-                </Box>
             </footer>
-        </>
     )
 }
 
