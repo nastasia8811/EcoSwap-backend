e8120d9d9e00ce49dd685a6e4ba0c02c17f2af6a
Melnyk Anastasiia <nastasia8811@gmail.com>
2024-06-05 13:15:11 +0200

    added button "add events"

--- client/src/pages/Events/Events.tsx
+++ client/src/pages/Events/Events.tsx
@@ -1,10 +1,21 @@
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
-import { Box, Container } from '@mui/material';
+import { Box, Container,} from '@mui/material';
 import './Events.scss';
 import EventItem from "src/components/EventItem/EventItem";
 import { eventsList } from "./eventlist";
-
-    
+import React from 'react';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+// import { createTheme } from '@mui/material/styles';
+// import {lightGreen} from '@mui/material/colors';
+//
+//
+// const theme = createTheme({
+//     palette: {
+//         primary: lightGreen,
+//
+//     },
+// });
 
 const Events:React.FC = () => {
 
@@ -14,6 +25,12 @@ const Events:React.FC = () => {
             <BreadCrumbs linksArray={[{ link:'/events', text: 'Events' }]} />
                 <div className="events-container__wrapper">
                     <h2 className="events-container__wrapper-title">Events</h2>
+                    <div className="events-container__wrapper-plus">
+                        <Fab  size="small" color="primary"
+                             aria-label="add">
+                            <AddIcon />
+                        </Fab></div>
+
                     <Box className="events-container__wrapper-flex">
                     {eventsList.map((item:any) => (
                          <EventItem key={item.id} id={item.id} title={item.title} img={item.img} />
