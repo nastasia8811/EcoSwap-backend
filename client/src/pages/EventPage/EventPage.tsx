c970bc9e06a17c0bcc40acffe071d0342fc38c49
Melnyk Anastasiia <nastasia8811@gmail.com>
2024-06-08 15:14:40 +0200

    we receive Event items from the server

--- client/src/pages/EventPage/EventPage.tsx
+++ client/src/pages/EventPage/EventPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
-import { Container , ToggleButton} from '@mui/material';
-import { eventsList } from '../Events/eventlist';
+//import { useParams } from 'react-router-dom';
+//import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+//import { Container , ToggleButton} from '@mui/material';
 import './EventPage.scss';
-
-
+//
+// import {useEffect, useState} from 'react';
+// import { useSelector } from 'react-redux';
 // @ts-ignore
 import background from './img/background.jpg';
 interface EventPageProps {
@@ -19,30 +19,35 @@ interface EventPageProps {
     onClick?: () => void;
 }
 const EventPage: React.FC<EventPageProps> = () => {
-    const { id } = useParams<{ id: string }>();
-    const event = eventsList.find(event => event.id === parseInt(id || '', 10));
-
-    if (!event) {
-        return <div>Event not found</div>;
-    }
+    // const [event, setEvent] = useState<any>(null);
+    // const { id } = useParams<{ id: string }>();
+    // const eventsData: EventPageProps[] = useSelector((state: any) => state.events.eventsData);
+    // useEffect(() => {
+    //     const event = eventsData.find(event => event.id === parseInt(id || '', 10));
+    //     setEvent(event);
+    // }, [id]);
+    //
+    // if (!event) {
+    //     return <div>Event not found</div>;
+    // }
 
     return (
         <>
-            <Container className="eventPage" maxWidth="xl">
-                <img className="eventPage__img" src={background} alt='nature'/>
-                <BreadCrumbs linksArray={[{ link: '/events', text: 'Events' }, { link: `/event/${id}`, text: 'Event' }]} />
-                <div className="eventPage__container">
+            {/*<Container className="eventPage" maxWidth="xl">*/}
+                {/*<img className="eventPage__img" src={background} alt='nature'/>*/}
+                {/*<BreadCrumbs linksArray={[{ link: '/events', text: 'Events' }, { link: `/event/${id}`, text: 'Event' }]} />*/}
+                {/*<div className="eventPage__container">*/}
 
-                    <h2 className="eventPage__container-title">{event.title}</h2>
-                    <div className="eventPage__container-date">{event.date}</div>
-                    <div className="eventPage__container-available">{event.available}</div>
+                {/*    <h2 className="eventPage__container-title">{event.title}</h2>*/}
+                {/*    <div className="eventPage__container-date">{event.date}</div>*/}
+                {/*    <div className="eventPage__container-available">{event.available}</div>*/}
 
-                    <ToggleButton value="android">Join Event</ToggleButton>
-                    <h4 className="eventPage__container-city">{event.city}</h4>
-                    <img className="eventPage__container-img" src={event.img} alt={event.title} />
-                    <p className="eventPage__container-description">{event.description}</p>
-                </div>
-            </Container>
+                {/*    <ToggleButton value="android">Join Event</ToggleButton>*/}
+                {/*    <h4 className="eventPage__container-city">{event.city}</h4>*/}
+                {/*    <img className="eventPage__container-img" src={event.img} alt={event.title} />*/}
+                {/*    <p className="eventPage__container-description">{event.description}</p>*/}
+                {/*</div>*/}
+            {/*</Container>*/}
         </>
     );
 };
