e19d42fc0579ce58ea50b057500f8e605d1ef0d3
Melnyk Anastasiia <nastasia8811@gmail.com>
2024-06-16 22:19:57 +0200

    cloudinary append

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,18 +3,19 @@ import './Header.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { logo } from "./icons";
 import { useState } from "react";
-//import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
-//import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-//import {selectorLoginUserData} from "../../selectors";
+import {selectorLoginToken} from "../../selectors";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import {actionResetState} from "../../reducers";
 
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    //const isUserAuthorized = useSelector()
-
+    const isUserAuthorized = useSelector(selectorLoginToken)
+const dispatch = useDispatch()
     return (
         <header className="header">
             <Container maxWidth="xl">
@@ -28,15 +29,13 @@ const Header = () => {
                         <NavLink to="/about" className="header__wrapper-menu-item">
                             About us
                         </NavLink>
-                        {/*<NavLink to="/blog" className="header__wrapper-menu-item">*/}
-                        {/*    Blog*/}
-                        {/*</NavLink>*/}
-                        {/*<NavLink to="/events" className="header__wrapper-menu-item">*/}
-                        {/*    Events*/}
-                        {/*</NavLink>*/}
-                        <NavLink to="/authorization" className="header__wrapper-menu-item">
-                            <LoginOutlinedIcon />
-                           {/*{isUserAuthorized ?<LoginOutlinedIcon /> : <AccountCircleOutlinedIcon /> }*/}
+
+                        <NavLink to="/events" className="header__wrapper-menu-item">
+                            Events
+                        </NavLink>
+                        <NavLink to="/authorization" className="header__wrapper-menu-item" onClick = {()=>{localStorage.removeItem('token'); dispatch(actionResetState(null))}}>
+
+                           {isUserAuthorized ?<LoginOutlinedIcon /> : <AccountCircleOutlinedIcon /> }
                         </NavLink>
 
                     </Box>
